923c7db39e9ed5118750dd08230b0f7879eca8e7
coldblade2000 <diegogranada7@gmail.com>
2023-08-21 20:04:30 -0500

    Attempted connection to database

--- Server/Utils/dataBase.js
+++ Server/Utils/dataBase.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-    host:'localhost',
+    host:'epilepsy-mysql',
     user: 'root',
     database: 'Ai-Epilepsy',
     password: 'Pipemerca410*'
