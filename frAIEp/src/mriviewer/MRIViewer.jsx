66c60b8d374a2e999c7f98cbf35053a41ebdc265
coldblade2000 <diegogranada7@gmail.com>
2023-10-11 02:41:23 -0500

    [feat] MRIViewer style adjustments and added missing assets

--- frAIEp/src/mriviewer/MRIViewer.jsx
+++ frAIEp/src/mriviewer/MRIViewer.jsx
@@ -7,6 +7,7 @@ import React from 'react';
 import { Main } from './ui/Main.jsx';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
+import "./MRIViewer.css"
 
 const MRIViewer = () => {
     return (
