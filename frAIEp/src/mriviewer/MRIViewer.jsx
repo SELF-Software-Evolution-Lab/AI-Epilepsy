82585ef9a0e30c4d428a991f8c7549a32f95953d
coldblade2000 <diegogranada7@gmail.com>
2023-11-22 02:18:11 -0500

    [feat] Fixed transaprent modal

--- frAIEp/src/mriviewer/MRIViewer.jsx
+++ frAIEp/src/mriviewer/MRIViewer.jsx
@@ -7,7 +7,6 @@ import React from 'react';
 import { Main } from './ui/Main.jsx';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
-import "./MRIViewer.css"
 import DebugControls from "./debug/DebugControls/DebugControls.jsx";
 
 const MRIViewer = () => {
