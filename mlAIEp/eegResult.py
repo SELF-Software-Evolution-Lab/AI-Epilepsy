83f384db9a53740b2479a5bbb45c82f50d587d08
Juan Pablo Carvajal Dossman <139716860+JuanCarvajalDossman@users.noreply.github.com>
2024-04-30 11:46:18 -0500

    Update eegResult.py

--- mlAIEp/eegResult.py
+++ mlAIEp/eegResult.py
@@ -8,7 +8,6 @@ def eegResult(Array):
     Names=['Fp1-F7', 'P7-T3', 'T3-T5', 'T5-O1', 'Fp2-F8', 'F8-T4', 'T4-T6', 'T6-O2', 'Fp1-F3', 'F3-C3', 'C3-P3', 'P3-O1', 'Fp2-F4', 'F4-C4', 'C4-P4', 'P4-O2', 'Fz-Cz', 'Cz-Pz']
     elements = (array.size/(FreqSampleo*5*18)) // 1
     length = elements*5*FreqSampleo
-    print('lenth' + str(length))
     for j in range(0, int(length), int(5*FreqSampleo)):
         info=[]
         for k in range (0,len(Names)):
