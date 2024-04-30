89df9b75be6a159fcb5c35f367cacb05d4364fb6
Juan Pablo Carvajal Dossman <139716860+JuanCarvajalDossman@users.noreply.github.com>
2024-04-30 11:45:49 -0500

    Update eegPrediciton.py

--- mlAIEp/eegPrediciton.py
+++ mlAIEp/eegPrediciton.py
@@ -2,8 +2,11 @@ def eegPrediciton(feature):
     import numpy as np
     import joblib
     import pandas as pd
-    model_path='pipelines/ModelAI.pkl'     # Model Location
-    scaler_path='pipelines/ScalerAI.pkl'   # Scaler Location
+    import os
+    current_dir = os.getcwd()
+    pipelinesLocation=os.path.join(current_dir, 'pipelines')
+    model_path=os.path.join(pipelinesLocation, 'ModelAI.pkl')     # Model Location
+    scaler_path=os.path.join(pipelinesLocation, 'ScalerAI.pkl')   # Scaler Location
     sc=joblib.load(scaler_path)
     model=joblib.load(model_path)
     value=pd.DataFrame(feature)
