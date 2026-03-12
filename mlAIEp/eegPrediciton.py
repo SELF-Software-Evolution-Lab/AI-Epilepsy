bed3ec71d78b36d5ea28d609207a5863db6d7271
Juan Pablo Carvajal Dossman <139716860+JuanCarvajalDossman@users.noreply.github.com>
2026-03-12 10:56:10 -0500

    Replace joblib model loading with Keras load_model

--- mlAIEp/eegPrediciton.py
+++ mlAIEp/eegPrediciton.py
@@ -1,6 +1,7 @@
 def eegPrediciton(feature):
     import numpy as np
     import joblib
+    from tensorflow.keras.models import load_model
     import pandas as pd
     import os
     current_dir = os.getcwd()
@@ -8,7 +9,7 @@ def eegPrediciton(feature):
     model_path=os.path.join(pipelinesLocation, 'pipelines', 'CNN1.h5')     # Model Location
     scaler_path=os.path.join(pipelinesLocation, 'pipelines', 'CNN1Scaler.pkl')   # Scaler Location
     sc=joblib.load(scaler_path)
-    model=joblib.load(model_path)
+    model=load_model(model_path)
     value=pd.DataFrame(feature)
     value=value.T
     value = sc.transform(value)
